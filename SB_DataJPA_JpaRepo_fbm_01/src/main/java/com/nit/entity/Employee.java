40cc6a1d4b17adb43bdc34cd1d12744240a45099
nit66 <nit66@192.168.1.9>
2024-12-06 19:15:47 +0530

    init

--- SB_DataJPA_JpaRepo_fbm_01/src/main/java/com/nit/entity/Employee.java
+++ SB_DataJPA_JpaRepo_fbm_01/src/main/java/com/nit/entity/Employee.java
@@ -4,7 +4,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
+//hghjfxhsafdahfdshfdsdhasfdsah
 @Entity
 @Table(name = "EMP_TBL")
 public class Employee {
